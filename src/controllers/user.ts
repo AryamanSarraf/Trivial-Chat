ea88265eb275a7ce95fee076e1c16d3adcce4a6d
vivek80801 <57554760+vivek80801@users.noreply.github.com>
2021-05-16 12:28:59 +0530

    adding little bit styling to midoker page

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -22,15 +22,37 @@ export const handleSignUp = (req: Request, res: Response) => {
   const newUser = new MyUser(name, username, email, password, confirmPassword);
   if (newUser.validate() === "valid user") {
     newUser.save();
-    res.send("<h1>Now you can log in </h1> <a href='/'>go back</a> to log in");
+    res.send(`
+<head>
+  <title>Trivia Chat Login</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="welcome.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</head>
+             <div style="padding:0.75rem">
+  <h1>Now you can log in </h1> 
+  <a href='/'>go back</a> to log in
+  </div>`);
   } else {
     const errors = newUser.validate();
     if (errors !== "valid user") {
       res.send(
-        `<h1>${errors.errors.map((err) => {
-          return err;
-        })}</h1>
-        <a href="/"> Go back </a> and try to sign up again`
+        `<div style=
+  'background-color: #c2c2c2; font-family:-apple-system,
+  BlinkMacSystemFont,
+  "Segoe UI",
+  "Roboto",
+  "Helvetica Neue", Arial, sans-serif,
+  "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol" !default;'>
+  <h1>${errors.errors.map((err) => {
+    return err;
+  })}</h1>
+        <a href="/"> Go back </a> and try to sign up again
+        </div>`
       );
     }
   }
